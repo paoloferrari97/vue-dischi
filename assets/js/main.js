5082d83c6d93c1805938054235004498a54751aa
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-28 12:50:09 +0200

    Richiamo api con axios e salvo dischi nei data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
+//Attraverso una chiamata ajax all’API di boolean https://flynn.boolean.careers/exercises/api/array/music 
+//avremo a disposizione una decina di dischi musicali.
+//Utilizzando vue, stampiamo a schermo una card per ogni album.
+
+//BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+
+//BONUS 2: Ordinare i dischi per anno di uscita.
+
+
 const app = new Vue({
     el: "#app",
     data: {
-        
+        url: "https://flynn.boolean.careers/exercises/api/array/music",
+        dischi:[]
     },
     methods: {
         
     },
     mounted() {
-        
+        axios
+        .get(this.url)
+        .then(resp => {
+            //console.log(resp);    
+            //console.log(resp.data.response);
+            this.dischi = resp.data.response;
+        })
+        .catch(e => {
+            console.log(e);
+        })
     }
 });
